18879d0631f282c17c0fe134332a809a6cc78096
Mark Stahl <mjstahl@gmail.com>
2019-09-09 11:49:04 -0400

    Pulled out error code into function

--- bin.js
+++ bin.js
@@ -22,6 +22,11 @@ function dateAndTitle (str) {
   ]
 }
 
+function exitWithError(err) {
+  console.error(err)
+  process.exit(1)
+}
+
 mkdirp.sync(BUILD_DIR)
 
 const posts = []
@@ -35,10 +40,8 @@ fs.readdirSync(CONTENT_DIR).map(file => {
 
   const postTemplate = path.join(TEMPLATE_DIR, 'post.ejs')
   ejs.renderFile(postTemplate, { body, date, title }, {}, (err, result) => {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
+    if (err) exitWithError(err)
+
     posts.push({ date, href: `${baseFilename}.html`, title })
 
     const outputFile = path.join(BUILD_DIR, `${baseFilename}.html`)
@@ -48,10 +51,8 @@ fs.readdirSync(CONTENT_DIR).map(file => {
 
 const listTemplate = path.join(TEMPLATE_DIR, 'list.ejs')
 ejs.renderFile(listTemplate, { posts }, {}, (err, result) => {
-  if (err) {
-    console.log(err)
-    process.exit(1)
-  }
+  if (err) exitWithError(err)
+
   const outputFile = path.join(BUILD_DIR, 'index.html')
   fs.writeFileSync(outputFile, result)
 })
